f3c1db51a4746ca332c26e021be95e21fcdcb502
Vishnu Gantam <vishnu_gantam@homedepot.com>
2020-02-20 13:32:21 -0500

    second commit

--- variables.tf
+++ variables.tf
@@ -4,33 +4,18 @@ variable "env" {
 variable "container_name" {
     description="name of the container"
     type="map"    
-    default={
-        prod="blog"
-        dev="dev_blog"
-    }
+
 }
 variable "image" {
     description="name of the image"
     type="map"
-    default={
-        prod="ghost:alpine"
-        dev="ghost:alpine"
-    }
+
 }
 variable "int_port" {
     description="internal port of the container"
     type="map"
-    default={
-        dev="2368"
-        prod="2368"
-        }
-  
 }
 variable "ext_port" {
     description="external port of the container"
     type="map"
-    default={
-        dev="80"
-        prod="8080"
-        }
 }
